c8b223ba6b1f80dd071f4d00122603934779fdc8
hminh2207 <phminh220702@gmail.com>
2023-07-02 22:37:37 +0700

    tạo controller cho phần tour và destination

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "web" middleware group. Make something great!
 |
-*/
+
 /*Destination*/
 Route::get('/add-destination','App\Http\Controllers\DestinationController@index');
 Route::post('/save-destination','App\Http\Controllers\DestinationController@add');
