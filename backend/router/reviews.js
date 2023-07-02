b4652e11b0d6a5191abfd00cde00c96b12c9ccc7
Đỗ Duy Khánh <92663363+trungdung1d@users.noreply.github.com>
2023-07-02 11:19:28 +0700

    Delete backend directory

--- backend/router/reviews.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import { createReview } from '../controllers/reviewController.js'
-import { verifyUser } from '../utils/verifyToken.js'
-
-const router = express.Router()
-
-router.post('/:tourID', verifyUser, createReview)
-
-export default router
