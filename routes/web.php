f5d966e2d478b49ebe9a523b83f970683278c0a1
trungdung1d <trungdungd84@gmail.com>
2023-07-03 00:12:02 +0700

    update file web.php Changes to be committed: 	modified:   routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-//
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
+*/
+Route::get('thankyou',function (){
+    return view('Page_views.BookingPage');
+});
 
 /*Destination*/
 Route::get('/add-destination','App\Http\Controllers\DestinationController@index');
@@ -61,7 +66,58 @@ Route::get('delete-tour/{id_tour}','App\Http\Controllers\TourController@delete')
 Route::get('edit-tour/{tour_id}','App\Http\Controllers\TourController@edit');
 Route::post('update-tour/{tour_id}','App\Http\Controllers\TourController@update');
 /*.........................................................................................................................*/
-
+/*Post*/
+Route::get('/add-post','App\Http\Controllers\PostController@index');
+Route::post('/save-post','App\Http\Controllers\PostController@add');
+Route::get('/list-post','App\Http\Controllers\PostController@show_list');
+Route::get('/edit-post/{post_id}','App\Http\Controllers\PostController@edit');
+Route::post('/update-post/{post_id}','App\Http\Controllers\PostController@update');
+Route::get('/delete-post/{post_id}','App\Http\Controllers\PostController@delete');
+Route::get('/enable-post/{post_id}','App\Http\Controllers\PostController@enable');
+Route::get('/disable-post/{post_id}','App\Http\Controllers\PostController@disable');
+/*.........................................................................................................................*/
+/*Custom*/
+Route::get('/list-custom/{status}','App\Http\Controllers\CustomController@show_list');
+Route::get('/details-custom/{custom_id}','App\Http\Controllers\CustomController@detail');
+Route::get('edit-custom/{custom_id}','App\Http\Controllers\CustomController@edit');
+Route::post('/update-custom','App\Http\Controllers\CustomController@update');
+/*Custom Contracts*/
+Route::get('/new-custom-contract','App\Http\Controllers\CustomContract@new_contract');
+Route::post('/add-custom-contract','App\Http\Controllers\CustomContract@add_contract');
+Route::post('/save-custom-contract','App\Http\Controllers\CustomContract@save_contract');
+Route::get('/list-custom-contract/{custom_contract_status}','App\Http\Controllers\CustomContract@list_contract');
+Route::get('/detail-custom-contract/{custom_contract_id}','App\Http\Controllers\CustomContract@detail_contract');
+Route::post('/update-custom-contract/{custom_contract_id}','App\Http\Controllers\CustomContract@update');
+/*.........................................................................................................................*/
+/*Customer post*/
+Route::get('/them-bai-viet','App\Http\Controllers\CustomerController@index');
+Route::post('/luu','App\Http\Controllers\CustomerController@add');
+/*.........................................................................................................................*/
+/*Dashboard*/
+Route::get('dashboard','App\Http\Controllers\HomeController@dashboard');
+Route::get('dashboardkd','App\Http\Controllers\HomeController@dashboardkd');
+Route::get('dashboardhd','App\Http\Controllers\HomeController@dashboardhd');
+Route::get('select-year','App\Http\Controllers\HomeController@select_year')->name('select_year');
+/*.........................................................................................................................*/
+/*Page views*/
+Route::get('','App\Http\Controllers\HomeController@index');
+Route::get('/destination/{destination_name_EN}','App\Http\Controllers\DestinationController@show_dest_detail');
+Route::get('destination','App\Http\Controllers\DestinationController@show_page');
+Route::get('post','App\Http\Controllers\PostController@show_page');
+Route::get('/post/{post_title}','App\Http\Controllers\PostController@show_post_detail');
+Route::get('/type-of-tour/{typetour_name_EN}','App\Http\Controllers\TypetourController@show_type_detail');
+Route::get('/type-of-tour','App\Http\Controllers\TypetourController@show_page');
+Route::get('tour/{tour_name_EN}','App\Http\Controllers\TourController@show_tour_detail');
+Route::get('book-form/{tour_name_VI}','App\Http\Controllers\TourController@book_form');
+Route::post('booking','App\Http\Controllers\BookingController@booking');
+Route::get('wishlist','App\Http\Controllers\CustomController@wishlist');
+Route::post('custom','App\Http\Controllers\CustomController@custom');
+Route::get('search-1','App\Http\Controllers\HomeController@Search_1');
+Route::get('search-2','App\Http\Controllers\HomeController@Search_2');
+Route::get('customer-review/{booking_code}','App\Http\Controllers\BookingController@customer_review');
+Route::post('save-customer-review','App\Http\Controllers\BookingController@save_customer_review');
+Route::get('tra-cuu/{booking_code}','App\Http\Controllers\BookingController@tra_cuu');
+/*.........................................................................................................................*/
 /*Login customer*/
 Route::get('/login-index','App\Http\Controllers\CustomerController@login_index');
 Route::post('/login-customer','App\Http\Controllers\CustomerController@login_customer');
